1341c00c57ffa91c9fb31cc1acf42cddb054fd64
nina2cool <christinalmcmillan@gmail.com>
2017-01-10 12:13:45 -0600

    updated readme, updated towers of hanoi, minor text changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nina2cool.github.io
+
+This is my online portfolio.  
+
+As of 1/6/2017, I am working on updating it and improving it in the hopes of being able to show off my skills and code better.
--- portfolio_js/TowersOfHanoi/js/script.js
+++ portfolio_js/TowersOfHanoi/js/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 $(document).ready(function() {
-    // Put app logic here
 
+    // start with zero turns, zero clicks.  I am going to track the clicks so I know if it is removing a block or adding a block.
     var numberOfTurns = 0;
     var numberOfClicks = 0;
     var $block = null;
@@ -20,14 +20,14 @@ $(document).ready(function() {
             var $thisStackLastChild = $thisStack.children().last();
             var $thisDataBlockValue = $thisStackLastChild.data('block');
 
-            var $thisLength = $(this).children().length;
+            var $thisStackLength = $(this).children().length;
 
             if ($thisDataBlockValue === undefined) {
                 $thisDataBlockValue = 0;
             }
 
             //if first click, then remove it
-            if ($thisLength > 0) {
+            if ($thisStackLength > 0) {
 
                 if (numberOfClicks === 0) {
                     $currentStack = $thisStackNumber;
@@ -35,10 +35,10 @@ $(document).ready(function() {
                     numberOfClicks++;
 
                     $priorDataBlockValue = $thisDataBlockValue;
-
                     return;
                 }
             } else {
+                $('#announce-error').addClass('announce-error-background');
                 $('#announce-error').text('Error - please select a valid stack');
             }
 
@@ -46,24 +46,25 @@ $(document).ready(function() {
             // if second click, then add it
 
             if (numberOfClicks === 1) {
-
                 //check if a legal move
-                if (($thisDataBlockValue === 0 || $thisDataBlockValue > $priorDataBlockValue) && $thisStackNumber != $currentStack) {
+
+                if (($thisDataBlockValue === 0 || $thisDataBlockValue > $priorDataBlockValue)) {
 
                     $thisStack.append($block);
                     $currentStack = null;
                     numberOfClicks = 0;
 
                     numberOfTurns++;
-
+                    $('#announce-error').removeClass('announce-error-background');
                     $('#announce-error').text('');
                     $('#announce-number-of-turns').text('Number of Turns: ' + numberOfTurns);
                     checkForWin();
                     return;
                 } else {
-
+                    // debugger;
+                    numberOfClicks = 1;
+                    $('#announce-error').addClass('announce-error-background');
                     $('#announce-error').text('Invalid move - stack block cannot be larger than existing block OR you must select an empty stack OR you selected the same stack');
-
                 }
             }
 
@@ -73,13 +74,12 @@ $(document).ready(function() {
 
 
 
-    }); // end of clicking the second stack
+    }); // end of clicking the stack
 
     function checkForWin() {
 
         var $lengthStackTwo = $('[data-stack="2"]').children().length;
         var $lengthStackThree = $('[data-stack="3"]').children().length;
-
 
         if ($lengthStackTwo === 4 || $lengthStackThree === 4) {
 
